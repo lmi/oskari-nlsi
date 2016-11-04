7a31f24ddae77e73253bd38b11fb24cf4b947190
Haflidi Sigtryggur Magnusson <hsm@localhost.localdomain>
2016-11-04 13:17:35 +0000

    fixed java_home

--- startup-scripts/oskari.sh
+++ startup-scripts/oskari.sh
@@ -8,9 +8,9 @@
 export COMPONENT=`basename $0|sed 's/\.sh$//g'`
 export RUN_USER=oskari
 export PIDFILE=/var/run/${RUN_USER}/${COMPONENT}.pid
-export LOGFILE=/var/log/${RUN_USER}/${COMPONENT}.log
+export LOGFILE=/var/log/${RUN_USER}/${COMPONENT}/std.log
 
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre
+export JAVA_HOME=/opt/jdk
 export JETTY_HOME=/opt/${RUN_USER}/${COMPONENT}
 export JAVA_EXT_LIB=/usr/local/share/java
 
